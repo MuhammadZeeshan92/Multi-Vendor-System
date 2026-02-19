8e387927a78273b8dbf781630da43e9585f8be78
ZeeDev204 <muhammadzeeshanzakir7@gmail.com>
2026-02-20 00:55:25 +0500

    env added

---
+++ Backend/models/Product.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    stock: {
+      type: Number,
+      required: true,
+    },
+
+    images: [
+      {
+        type: String,
+      },
+    ],
+
+    vendor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
